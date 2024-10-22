6f2846dae124b3784d76186a6e128c2c0b1215a8
Bruno <brmartins92@gmail.com>
2024-10-22 18:17:47 -0300

    implemented fetchs questions of comments and deletes

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -2,9 +2,28 @@ import { AnswerCommentsRepository } from '@/domain/forum/application/repositorie
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
 
 export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepository {
-    public items: AnswerComment[] = [];
-
-    async create(answerComment: AnswerComment) {
-        this.items.push(answerComment);
+    public items: AnswerComment[] = []
+  
+    async findById(id: string) {
+      const answerComment = this.items.find((item) => item.id.toString() === id)
+  
+      if (!answerComment) {
+        return null
+      }
+  
+      return answerComment
     }
-}
+  
+    async create(answerComment: AnswerComment) {
+      this.items.push(answerComment)
+    }
+  
+    async delete(answerComment: AnswerComment) {
+      const itemIndex = this.items.findIndex(
+        (item) => item.id === answerComment.id,
+      )
+  
+      this.items.splice(itemIndex, 1)
+    }
+  }
+  
